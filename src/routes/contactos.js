94f04b7f56e9c606a3e4570991bb180f0eef43ea
emathedog <emanuel.gomez@hospitalitaliano.org.ar>
2020-06-16 20:01:57 -0300

    revistas

--- src/routes/contactos.js
+++ src/routes/contactos.js
@@ -1,40 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const daoContacto = require('../dao/dao_contactos')
+const daoContacto = require("../dao/dao_contactos");
 
-router.get('/contactos', async (req, res) => {
-  const contactos = await daoContacto.listarContactos() ;
-  res.render('v_contactos/v_contactos', {
-      contactos
+router.get("/contactos", async (req, res) => {
+  const contactos = await daoContacto.listarContactos();
+  res.render("v_contactos/v_contactos", {
+    contactos,
   });
 });
 
-router.post('/contactos',  (req, res, next) => {
-   daoContacto.guardarContacto(req.body)
-  res.redirect('/contactos')
+router.post("/contactos", (req, res, next) => {
+  daoContacto.guardarContacto(req.body);
+  res.redirect("/contactos");
 });
 
-router.get('/eliminar/:id', async (req,res)=>{
-  const {id} = req.params
-  await daoContacto.borrarContacto(id);
-  res.redirect('/contactos')
-});
-
-router.get('/editar/:id',async(req,res)=>{
-  const {id} = req.params
- const contactos = await daoContacto.buscarContacto(id)
-  res.render('v_contactos/v_contactos_edit',{
-    contactos
-  });
-
-});
-
-
-router.post('/editar/:id', async (req, res) => {
+router.get("/eliminar/:id", async (req, res) => {
   const { id } = req.params;
-  daoContacto.actualizarContacto(id,req.body);
-  res.redirect('/contactos');
+  await daoContacto.borrarContacto(id);
+  res.redirect("/contactos");
 });
 
+router.get("/editar/:id", async (req, res) => {
+  const { id } = req.params;
+  const contactos = await daoContacto.buscarContacto(id);
+  res.render("v_contactos/v_contactos_edit", {
+    contactos,
+  });
+});
+
+router.post("/editar/:id", async (req, res) => {
+  const { id } = req.params;
+  daoContacto.actualizarContacto(id, req.body);
+  res.redirect("/contactos");
+});
 
 module.exports = router;
--- src/routes/revistas.js
+++ src/routes/revistas.js
@@ -1,48 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const daoRevistda = require('../dao/dao_revistas')
-const Revistas = require('../model/revistas');
+const daoRevista = require('../dao/dao_revistas')
+
 
 
 
   router.get('/revistas', async (req, res) => {
-    const revistas = await Revistas.find();
+    const revistas = await daoRevista.listarRevistas();
     res.render('v_revistas/v_revistas', {
         revistas
     });
   });
   
   router.post('/revistas', async (req, res, next) => {
-    console.log(req.body)
-    const revistas = new Revistas(req.body);
-    await revistas.save();
+   
+   await daoRevista.guardarRevista(req.body)
+   
     res.redirect('/revistas');
   });
-  
-  router.get('/turn2/:id', async (req, res, next) => {
-    let { id } = req.params;
-    const revista = await Revistas.findById(id);
-    revista.status = !revistas.status;
-    await revista.save();
-    res.redirect('/revistas');
-  });
-  
+    
   
   router.get('/revista_edit/:id', async (req, res, next) => {
-    const revista = await Revistas.findById(req.params.id);
+    const revista = await daoRevista.buscarRevista(req.params.id);
     console.log(revista)
     res.render('v_revistas/v_revista_edit', { revista });
   });
   
   router.post('/revista_edit/:id', async (req, res, next) => {
     const { id } = req.params;
-    await Revistas.update({_id: id}, req.body);
+    await daoRevista.actualizarRevista(id,req.body)
     res.redirect('/revistas');
   });
   
   router.get('/delete2/:id', async (req, res, next) => {
     let { id } = req.params;
-    await Revistas.remove({_id: id});
+    await daoRevista.borrarRevista(id)
     res.redirect('/revistas');
   });
   
